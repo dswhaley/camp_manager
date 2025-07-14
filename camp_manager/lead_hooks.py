ae731067664b0023fd43ebd753d15974fc497cb5
dswhaley <whaleyds22@gcc.edu>
2025-07-14 17:21:29 -0400

    Changed how Camp and other organizations were being created, now uses new_doc instead of get_doc, also moved the creation of customers and onboardings to a different file

--- camp_manager/lead_hooks.py
+++ camp_manager/lead_hooks.py
@@ -12,57 +12,21 @@ def convert_lead_to_camp_and_customer(doc):
     if doc.custom_organization_type == "Camp":
         if not frappe.db.exists("Camp", {"organization_name": doc.company_name}):
             frappe.logger().info(f"Creating Camp for {doc.company_name}")
-            camp = frappe.get_doc({
-                "doctype": "Camp",
-                "organization_name": doc.company_name,
-                "contact_name": doc.custom_contact_name,
-                "email": doc.email_id,
-                "phone": doc.phone
-            })
-            camp.insert(ignore_permissions=True)
-
-        if not frappe.db.exists("Customer", {"customer_name": doc.company_name}):
-            frappe.logger().info(f"Creating Customer for {doc.company_name}")
-            customer = frappe.get_doc({
-                "doctype": "Customer",
-                "customer_name": doc.company_name,
-                "lead_name": doc.name,
-                "custom_camp_link": doc.company_name,
-                "customer_type": "Company"
-            })
-            customer.insert(ignore_permissions=True)
+            camp = frappe.new_doc("Camp")
+            camp.organization_name = doc.company_name
+            camp.contact_name = doc.custom_contact_name
+            camp.email = doc.email_id
+            camp.phone = doc.phone
+            camp.lead_link = doc.name
+            camp.save(ignore_permissions=True)
     else:
-        if not frappe.db.exists("Other_Organization", {"organization_name": doc.company_name}):
+        if not frappe.db.exists("Other Organization", {"organization_name": doc.company_name}):
             frappe.logger().info(f"Creating Camp for {doc.company_name}")
-            camp = frappe.get_doc({
-                "doctype": "Other Organization",
-                "organization_name": doc.company_name,
-                "contact_name": doc.custom_contact_name,
-                "email": doc.email_id,
-                "phone": doc.phone
-            })
-            camp.insert(ignore_permissions=True)
-
-        if not frappe.db.exists("Customer", {"customer_name": doc.company_name}):
-            frappe.logger().info(f"Creating Customer for {doc.company_name}")
-            customer = frappe.get_doc({
-                "doctype": "Customer",
-                "customer_name": doc.company_name,
-                "lead_name": doc.name,
-                "custom_other_organization_link": doc.company_name,
-                "customer_type": "Company"
-            })
-            customer.insert(ignore_permissions=True)
-
-    if not frappe.db.exists("Onboarding", {"title": doc.company_name}):
-        onboarding = frappe.get_doc({
-            "doctype": "Onboarding",
-            "name": doc.company_name,
-            "title": doc.company_name,
-            "organization_type": doc.custom_organization_type
-        })
-        if doc.custom_organization_type != "Camp":
-            onboarding.registration_identified = 1
-            onboarding.first_day_of_camp_provided = 1
-        onboarding.insert(ignore_permissions=True)
+            other_org = frappe.new_doc("Other Organization")
+            other_org.organization_name = doc.company_name
+            other_org.contact_name = doc.custom_contact_name
+            other_org.email = doc.email_id
+            other_org.phone = doc.phone
+            other_org.lead_link = doc.name
+            other_org.save(ignore_permissions=True)
     frappe.db.set_value("Lead", doc.name, "custom_converted_to_customer", 1)
