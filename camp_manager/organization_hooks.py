d7f65df0c3e8da7e2cfc1e889ea7e1de2f7dc8db
dswhaley <whaleyds22@gcc.edu>
2025-07-18 10:00:48 -0400

    Added popups for when Onboarding and Customer doctypes are created automatically

--- camp_manager/organization_hooks.py
+++ camp_manager/organization_hooks.py
@@ -13,7 +13,7 @@ def organization_creation(doc, method):
                     customer.custom_camp_link = doc.organization_name
                     customer.customer_type = "Company"
                     customer.save(ignore_permissions=True)
-
+                    frappe.msgprint(f"Customer {customer.name} created")
             elif doc.doctype == "Other Organization":
                 if not frappe.db.exists("Customer", {"customer_name": doc.organization_name}):
                     frappe.logger().info(f"Creating Customer for {doc.organization_name}")
@@ -23,7 +23,7 @@ def organization_creation(doc, method):
                     customer.custom_other_organization_link = doc.organization_name
                     customer.customer_type = "Company"
                     customer.save(ignore_permissions=True)
-            print("Checking if onboarding exists")
+                    frappe.msgprint(f"Customer {customer.name} created")
             if not frappe.db.exists("Onboarding", {"title": doc.organization_name}):
                 onboarding = frappe.new_doc("Onboarding")
                 onboarding.name = doc.organization_name  
@@ -34,6 +34,6 @@ def organization_creation(doc, method):
                     onboarding.first_day_of_camp_provided = 1
                 onboarding.save(ignore_permissions=True)
                 frappe.db.set_value(doc.doctype, doc.organization_name, "customer_and_onboarding_created", 1)
-                
+                frappe.msgprint(f"Onboarding document for {onboarding.name} created")
         except Exception as e:
             frappe.msgprint(f"Failed to create Customer and Onboarding due to: {str(e)}")
