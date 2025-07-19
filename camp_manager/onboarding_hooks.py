c2dc4ad1fef45b1f2e538a75506b9fa2d8a90ef7
dswhaley <whaleyds22@gcc.edu>
2025-07-19 17:22:59 -0400

    Made the Onboarding Checkboxes update based on information being present in the Camp/Other Organization, not whetehr the onboarding field had been filled in

--- camp_manager/onboarding_hooks.py
+++ camp_manager/onboarding_hooks.py
@@ -50,23 +50,26 @@ def update_camp(doc):
             # Registration Method
             if camp.registration_software != doc.registration_method and doc.registration_method:
                 camp.registration_software = doc.registration_method
+            if camp.registration_software:
                 doc.registration_identified = 1
 
             # Tax Info
-            if camp.tax_exempt != doc.exempt_status:
+            if camp.tax_exempt != doc.exempt_status and doc.exempt_status != "Pending":
                 camp.tax_exempt = doc.exempt_status
             if camp.tax_exemption_number != doc.tax_exempt_id:
                 camp.tax_exemption_number = doc.tax_exempt_id
-            if doc.exempt_status == "Taxed" or (doc.exempt_status and doc.tax_exempt_id):
+            if camp.tax_exempt == "Taxed" or (camp.exempt_status == "Exempt" and camp.tax_exemption_number):
                 doc.tax_exempt_id_gathered = 1
 
             # First Day of Camp
             if camp.first_day_of_camp != doc.first_day_of_camp and doc.first_day_of_camp:
                 camp.first_day_of_camp = doc.first_day_of_camp
+            if camp.first_day_of_camp:
                 doc.first_day_of_camp_provided = 1
 
             if camp.association != doc.custom_discount and doc.custom_discount:
                 camp.association = doc.custom_discount
+            if camp.association:
                 doc.custom_set_discount = 1
 
             # Shipping Address
@@ -92,34 +95,37 @@ def update_camp(doc):
                 doc.collected_address = 1
 
             # POC Info
-            if camp.contact_name != doc.poc_name:
+            if camp.contact_name != doc.poc_name and doc.poc_name:
                 camp.contact_name = doc.poc_name
-            if camp.email != doc.poc_email:
+            if camp.email != doc.poc_email and doc.email:
                 camp.email = doc.poc_email
-            if camp.phone != doc.poc_phone_number:
+            if camp.phone != doc.poc_phone_number and doc.poc_phone_number:
                 camp.phone = doc.poc_phone_number
-            if doc.poc_name and doc.poc_email and doc.poc_phone_number:
+            if camp.contact_name and camp.email and camp.phone:
                 doc.gathered_poc_information = 1
 
-            if camp.funfangle_username != doc.funfangle_username:
+            if camp.funfangle_username != doc.funfangle_username and doc.funfangle_username:
                 camp.funfangle_username = doc.funfangle_username
-            if camp.funfangle_password != doc.funfangle_password:
+            if camp.funfangle_password != doc.funfangle_password and doc.funfangle_password:
                 camp.funfangle_password = doc.funfangle_password
-            if doc.funfangle_password and doc.funfangle_username:
+            if camp.funfangle_password and camp.funfangle_username:
                 doc.account_setup = 1
             
 
-            if camp.link_to_parent_portal != doc.link_to_parent_portal:
+            if camp.link_to_parent_portal != doc.link_to_parent_portal and doc.link_to_parent_portal:
                 camp.link_to_parent_portal = doc.link_to_parent_portal
+            if camp.link_to_parent_portal:
                 doc.set_up_parent_portal = 1
 
             # Organization ID
             if camp.organization_order_id != doc.organization_order_id and doc.organization_order_id:
                 camp.organization_order_id = doc.organization_order_id
+            if camp.organization_order_id:
                 doc.assigned_organization_order_id = 1
 
             if camp.organization_funfangle_id != doc.organization_funfangle_id and doc.organization_funfangle_id:
                 camp.organization_funfangle_id = doc.organization_funfangle_id
+            if camp.organization_funfangle_id:
                 doc.assigned_organization_funfangle_id = 1
 
             camp.save(ignore_permissions=True)
@@ -156,15 +162,16 @@ def update_organization(doc):
                 other_organization = frappe.get_doc("Other Organization", doc.title)
 
             # Tax Info
-            if original.exempt_status != doc.exempt_status:
+            if original.exempt_status != doc.exempt_status and doc.exempt_status != "Pending":
                 other_organization.tax_exempt = doc.exempt_status
-            if original.tax_exempt_id != doc.tax_exempt_id:
+            if original.tax_exempt_id != doc.tax_exempt_id and doc.tax_exempt_id:
                 other_organization.tax_exemption_number = doc.tax_exempt_id
-            if doc.exempt_status == "Taxed" or (doc.exempt_status and doc.tax_exempt_id):
+            if other_organization.tax_exempt == "Taxed" or (other_organization.tax_exempt and other_organization.tax_exemption_number):
                 doc.tax_exempt_id_gathered = 1
 
             if original.custom_discount != doc.custom_discount and doc.custom_discount:
                 other_organization.association = doc.custom_discount
+            if other_organization.association:
                 doc.custom_set_discount = 1
 
             # Shipping Address
@@ -191,34 +198,37 @@ def update_organization(doc):
                 doc.collected_address = 1
 
             # POC Info
-            if original.poc_name != doc.poc_name:
+            if original.poc_name != doc.poc_name and doc.poc_name:
                 other_organization.contact_name = doc.poc_name
-            if original.poc_email != doc.poc_email:
+            if original.poc_email != doc.poc_email and doc.poc_email:
                 other_organization.email = doc.poc_email
-            if original.poc_phone_number != doc.poc_phone_number:
+            if original.poc_phone_number != doc.poc_phone_number and doc.poc_phone_number:
                 other_organization.phone = doc.poc_phone_number
-            if doc.poc_name and doc.poc_email and doc.poc_phone_number:
+            if other_organization.contact_name and other_organization.email and other_organization.phone:
                 doc.gathered_poc_information = 1
 
             # Organization ID
             if original.organization_order_id != doc.organization_order_id and doc.organization_order_id:
                 other_organization.organization_order_id = doc.organization_order_id
+            if other_organization.organization_order_id:
                 doc.assigned_organization_order_id = 1
 
             if original.organization_funfangle_id != doc.organization_funfangle_id and doc.organization_funfangle_id:
                 other_organization.organization_funfangle_id = doc.organization_funfangle_id
+            if other_organization.organization_funfangle_id:
                 doc.assigned_organization_funfangle_id = 1
 
-            if other_organization.funfangle_username != doc.funfangle_username:
+            if other_organization.funfangle_username != doc.funfangle_username and doc.funfangle_username:
                 other_organization.funfangle_username = doc.funfangle_username
-            if other_organization.funfangle_password != doc.funfangle_password:
+            if other_organization.funfangle_password != doc.funfangle_password and doc.funfangle_password:
                 other_organization.funfangle_password = doc.funfangle_password
-            if doc.funfangle_password and doc.funfangle_username:
+            if other_organization.funfangle_password and other_organization.funfangle_username:
                 doc.account_setup = 1
             
 
-            if other_organization.link_to_parent_portal != doc.link_to_parent_portal:
+            if other_organization.link_to_parent_portal != doc.link_to_parent_portal and doc.link_to_parent_portal:
                 other_organization.link_to_parent_portal = doc.link_to_parent_portal
+            if other_organization.link_to_parent_portal:
                 doc.set_up_parent_portal = 1
             other_organization.save(ignore_permissions=True)
 
